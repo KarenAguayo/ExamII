48c20d5d3ced93d539142dbda2e73470b0161148
karen <karen.aguayo@ucb.edu.bo>
2022-04-12 12:05:56 -0400

    bisonte aun no pasa y termino el tiempo

---
+++ src/bisonte.js
@@ -0,0 +1,11 @@
+function CompararBisonte(codigo)
+{
+    var bisonte = '%';
+        for(var i = 0; (i/codigo==0&&i/i==0).length; i++)
+        {
+                    bisonte;
+        }
+        return bisonte;
+}
+
+export default CompararBisonte; 
